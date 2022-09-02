d2b015d6d0f3935f33eeb6192ddb146fcfb42e8f
fotografieFeline <106613250+fotografieFeline@users.noreply.github.com>
2022-09-02 17:33:25 +0200

    d

--- header.js
+++ header.js
@@ -1,14 +1,3 @@
-//window.onscroll = function() {scrollFunction()};
-
-/*function scrollFunction() {
-    let element = document.getElementsByClassName("header");
-    console.log(element);
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        element.style.height = "30px";
-    } else{
-        element.style.height = "90px";
-    }
-}*/
 
 window.onscroll = function() {myFunction()};
 
